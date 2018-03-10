9124f150ddb96c4623fd53247868a735b8690a0b
Darshil Shah <darshil.saphcm@gmail.com>
2018-03-10 11:54:58 +0530

    Display Data in a ListView

--- app/src/main/java/com/example/x_men/storage/MainActivity.java
+++ app/src/main/java/com/example/x_men/storage/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.x_men.storage;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.x_men.storage.model.DataItem;
 import com.example.x_men.storage.sample.SampleDataProvider;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -14,27 +17,40 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     @SuppressWarnings("FieldCanBeLocal")
-    private TextView tvOut;
+//    private TextView tvOut;
     List<DataItem> dataItemList = SampleDataProvider.dataItemList;
+    List<String> itemNames = new ArrayList<> ();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvOut = (TextView) findViewById(R.id.out);
-        tvOut.setText("");
+//        tvOut = (TextView) findViewById(R.id.out);
+//        tvOut.setText("");
 
-        Collections.sort (dataItemList, new Comparator<DataItem> () {
-            @Override
-            public int compare(DataItem o1, DataItem o2) {
-                return o1.getItemName ().compareTo (o2.getItemName ());
-            }
-        });
+//        Collections.sort (dataItemList, new Comparator<DataItem> () {
+//            @Override
+//            public int compare(DataItem o1, DataItem o2) {
+//                return o1.getItemName ().compareTo (o2.getItemName ());
+//            }
+//        });
 
         for (DataItem item : dataItemList){
-            tvOut.append (item.getItemName () + "\n");
+//            tvOut.append (item.getItemName () + "\n");
+            itemNames.add(item.getItemName ());
         }
+
+        Collections.sort (itemNames);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String> (
+            this, android.R.layout.simple_list_item_1, itemNames);
+
+        ListView listView = findViewById (android.R.id.list);
+        listView.setAdapter (adapter);
+
+
+
 
     }
 }
