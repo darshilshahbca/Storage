d3160ebc4cf9b9a5a7db1cccf3bac293e034ca15
Darshil Shah <darshil.saphcm@gmail.com>
2018-03-10 21:48:50 +0530

    Manage Shared Prefrences though JAVA

--- app/src/main/java/com/example/x_men/storage/MainActivity.java
+++ app/src/main/java/com/example/x_men/storage/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.x_men.storage;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.x_men.storage.model.DataItem;
 import com.example.x_men.storage.sample.SampleDataProvider;
@@ -14,25 +19,57 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    @SuppressWarnings("FieldCanBeLocal")
+    private static final int SIGNIN_REQUEST = 1001;
+    public static final String MY_GLOBAL_PREFS = "my_global_prefs";
     List<DataItem> dataItemList = SampleDataProvider.dataItemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+         setContentView(R.layout.activity_main);
 
-        Collections.sort (dataItemList, new Comparator<DataItem> () {
+        Collections.sort(dataItemList, new Comparator<DataItem>() {
             @Override
             public int compare(DataItem o1, DataItem o2) {
-                return o1.getItemName ().compareTo (o2.getItemName ());
+                return o1.getItemName().compareTo(o2.getItemName());
             }
         });
 
-        DataItemAdapter adapter = new DataItemAdapter (this, dataItemList);
+        DataItemAdapter adapter = new DataItemAdapter(this, dataItemList);
 
-        RecyclerView recyclerView = findViewById (R.id.rvItems);
-        recyclerView.setAdapter (adapter);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rvItems);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_signin:
+                Intent intent = new Intent(this, SigninActivity.class);
+                startActivityForResult(intent, SIGNIN_REQUEST);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == SIGNIN_REQUEST) {
+            String email = data.getStringExtra(SigninActivity.EMAIL_KEY);
+            Toast.makeText(this, "You signed in as " + email, Toast.LENGTH_SHORT).show();
+
+            SharedPreferences.Editor editor =
+                    getSharedPreferences (MY_GLOBAL_PREFS, MODE_PRIVATE).edit ();
+            editor.putString (SigninActivity.EMAIL_KEY, email);
+            editor.apply();
+        }
 
     }
 }
