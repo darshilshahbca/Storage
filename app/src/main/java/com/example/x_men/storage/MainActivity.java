3751002f9cfd46d914c4efeedfb8e74270467748
Darshil Shah <darshil.saphcm@gmail.com>
2018-03-10 11:16:12 +0530

    Added Model Data in POJO Class

--- app/src/main/java/com/example/x_men/storage/MainActivity.java
+++ app/src/main/java/com/example/x_men/storage/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.example.x_men.storage.model.DataItem;
+
 public class MainActivity extends AppCompatActivity {
 
     @SuppressWarnings("FieldCanBeLocal")
@@ -14,8 +16,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DataItem item =
+                new DataItem (null, "My menu item", "a description", "a category", 1, 9.95, "apple_pie.jpg"   );
+
         tvOut = (TextView) findViewById(R.id.out);
-        tvOut.setText(R.string.ready_to_code);
+        tvOut.setText(item.toString ());
 
     }
 }
