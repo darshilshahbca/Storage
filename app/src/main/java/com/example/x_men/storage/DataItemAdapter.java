621c176ec923f239e1e3db1a2fb2f38e5ec2071c
Darshil Shah <darshil.saphcm@gmail.com>
2018-03-10 15:01:06 +0530

    Pass Intent Extra to other activity

--- app/src/main/java/com/example/x_men/storage/DataItemAdapter.java
+++ app/src/main/java/com/example/x_men/storage/DataItemAdapter.java
@@ -1,6 +1,7 @@
 package com.example.x_men.storage;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -18,6 +19,7 @@ import java.util.List;
 
 public class DataItemAdapter extends RecyclerView.Adapter<DataItemAdapter.ViewHolder> {
 
+    public static final String ITEM_ID_KEY = "item_id_key";
     private List<DataItem> mItems;
     private Context mContext;
 
@@ -51,7 +53,11 @@ public class DataItemAdapter extends RecyclerView.Adapter<DataItemAdapter.ViewHo
         holder.mView.setOnClickListener (new View.OnClickListener (){
             @Override
             public void onClick(View view) {
-                Toast.makeText (mContext, "You Selected : " + item.getItemName (), Toast.LENGTH_SHORT).show ();
+//                Toast.makeText (mContext, "You Selected : " + item.getItemName (), Toast.LENGTH_SHORT).show ();
+                String itemId = item.getItemId ();
+                Intent intent = new Intent(mContext, DetailActivity.class);
+                intent.putExtra (ITEM_ID_KEY, itemId);
+                mContext.startActivity (intent);
             }
         });
 
